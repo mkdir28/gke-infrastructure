ed03709c00678f86968270b6e38691d7f5584f1b
Maryna Kamienieva <111189785+mkdir28@users.noreply.github.com>
2026-06-11 11:05:01 +0000

    add init terraform

---
+++ terraform/variables.tf
@@ -0,0 +1,9 @@
+variable "GOOGLE_REGION" {
+  type        = string
+  description = "GCP region to use"
+}
+
+variable "GOOGLE_PROJECT" {
+  type        = string
+  description = "GCP project name"
+}
